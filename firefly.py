949f633df30e7a84f648580af4e26fcba70dbd9a
Michael Weinberg <mweinberg@nyu.edu>
2020-07-31 08:07:18 -0400

    added brimleyi pattern function (use as new template) + random offset variable

--- firefly.py
+++ firefly.py
@@ -1,7 +1,10 @@
+#https://www.nps.gov/grsm/learn/nature/firefly-flash-patterns.htm
+
 import board
 import digitalio
 import time
 import neopixel
+import random
 
 #create the neopixel. auto_write=True avoids having to push changes (at the cost of speed, which probably doesn't matter here)
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness = 0.2, auto_write=False)
@@ -16,6 +19,9 @@ neo_b = 0
 action_time_1 = time.monotonic()
 action_time_2 = time.monotonic()
 
+reset_time_2 =  time.monotonic()
+reset_time_3  = time.monotonic()
+
 
 def pattern_one(action_time_input, light_number):
     #First LED
@@ -26,20 +32,14 @@ def pattern_one(action_time_input, light_number):
     if time_from_action_1 < 9:
         pixels[light_number] = (0, 0, 0)
         pixels.show()
-        print(time_from_action_1)
-        print("0000000000000000000000")
         action_time =  action_time_input
     if time_from_action_1 > 9 and time_from_action_1 < 20:
         pixels[light_number] = (neo_r, neo_g, neo_b)
         pixels.show()
-        print(time_from_action_1)
-        print("1111111111111111111111")
         action_time =  action_time_input
     if time_from_action_1 > 20:
         pixels[light_number] = (0, 0, 0)
         pixels.show()
-        print(time_from_action_1)
-        print("2222222222222222222222")
         #this is the end of the cycle so the time counting is reset
         action_time = time.monotonic()
     return action_time
@@ -78,11 +78,47 @@ def pattern_two(action_time_input, light_number):
         action_time = time.monotonic()
     return action_time
 
+def brimleyi(reset_time_input, light_number):
+    #calculates how much time has passed since the new zero
+    time_from_zero = time.monotonic() - reset_time_input
+    # creates the carry over reset_time variable so that it can be returned even if it is not updated in the last if statement
+    reset_time = reset_time_input
+
+    # on flash
+    if 5 <= time_from_zero <= 5.5:
+        pixels[light_number] = (neo_r, neo_g, neo_b)
+        pixels.show()
+        print("b1")
+        print(time_from_zero)
+    elif 15 <= time_from_zero <= 15.5:
+        pixels[light_number] = (neo_r, neo_g, neo_b)
+        pixels.show()
+        print("b2")
+        print(time_from_zero)
+
+    # reset (includes 10 seconds after second flash - 5 on the back end and 5 on the front end)
+    elif time_from_zero > 20:
+        pixels[light_number] = (0, 0, 0)
+        pixels.show()
+        reset_time = time.monotonic() + random.uniform(-3, 3)
+
+    # all of the off times
+    else:
+        pixels[light_number] = (0, 0, 0)
+        pixels.show()
+        print("b000000")
+        print(time_from_zero)
+
+    return reset_time
+
 
 while True:
 
     action_time_1 = pattern_one(action_time_1, 0)
     action_time_2 = pattern_two(action_time_2, 1)
+    reset_time_2 = brimleyi(reset_time_2, 2)
+    reset_time_3 = brimleyi(reset_time_3, 3)
+
 
 
 
