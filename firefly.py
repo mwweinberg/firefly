a4baeec89bcc06efa4ab82bc3fe9735de4a6e8cd
Michael Weinberg <mweinberg@nyu.edu>
2020-07-31 08:23:30 -0400

    added macdermotti

--- firefly.py
+++ firefly.py
@@ -16,11 +16,13 @@ neo_b = 0
 
 
 #instantite the 'zero' counter
+#right now you need on of these (reset_time_x) for each light
 action_time_1 = time.monotonic()
 action_time_2 = time.monotonic()
 
-reset_time_2 =  time.monotonic()
-reset_time_3  = time.monotonic()
+reset_time_2 = time.monotonic()
+reset_time_3 = time.monotonic()
+reset_time_4 = time.monotonic()
 
 
 def pattern_one(action_time_input, light_number):
@@ -111,13 +113,45 @@ def brimleyi(reset_time_input, light_number):
 
     return reset_time
 
+def macdermotti (reset_time_input, light_number):
+    #calculates how much time has passed since the new zero
+    time_from_zero = time.monotonic() - reset_time_input
+    # creates the carry over reset_time variable so that it can be returned even if it is not updated in the last if statement
+    reset_time = reset_time_input
+
+    # on flash
+    if 3 <= time_from_zero <= 3.5:
+        pixels[light_number] = (neo_r, neo_g, neo_b)
+        pixels.show()
+    elif 5 <= time_from_zero <= 5.5:
+        pixels[light_number] = (neo_r, neo_g, neo_b)
+        pixels.show()
+    elif 10 <= time_from_zero <= 10.5:
+        pixels[light_number] = (neo_r, neo_g, neo_b)
+        pixels.show()
+    elif 12 <= time_from_zero <= 12.5:
+        pixels[light_number] = (neo_r, neo_g, neo_b)
+        pixels.show()
+
+    elif time_from_zero > 14.5:
+        pixels[light_number] = (0, 0, 0)
+        pixels.show()
+        reset_time = time.monotonic() + random.uniform(-3, 3)
+
+    else:
+        pixels[light_number] = (0, 0, 0)
+        pixels.show()
+
+    return reset_time
 
 while True:
 
     action_time_1 = pattern_one(action_time_1, 0)
     action_time_2 = pattern_two(action_time_2, 1)
+
     reset_time_2 = brimleyi(reset_time_2, 2)
     reset_time_3 = brimleyi(reset_time_3, 3)
+    reset_time_4 = macdermotti(reset_time_4, 4)
 
 
 
